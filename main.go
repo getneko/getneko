7ef105d1590d557b665f80c08b8aaf426a70a8e3
cangkuai <admin@ckapp.cn>
2024-03-03 20:59:23 +0800

    feat:user can set port by flag

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	_ "getneko/docs"
 	"getneko/router"
+	"strconv"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -19,11 +20,15 @@ func main() {
 	//根据flag导入swagger依赖
 	var enabledoc bool
 	flag.BoolVar(&enabledoc, "doc", false, "enable swagger doc")
+	//指定端口
+	var flagports int
+	flag.IntVar(&flagports, "port", 61223, "set server port")
 	flag.Parse()
+	runports := ":" + strconv.Itoa(flagports)
 	if enabledoc {
 		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	}
-	err := r.Run()
+	err := r.Run(runports)
 	if err != nil {
 		panic(err)
 	}
